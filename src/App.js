4bdc055c2abfec5b29db584900c8740107fe75df
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-03-16 19:25:44 -0300

    instala reactBoostrap y los estilos de boostrap para desarrollo del proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Esto es un titulo</h1>
+    <div>
+      <h1 className="display-4">Esto es un titulo</h1>
     </div>
   );
 }
