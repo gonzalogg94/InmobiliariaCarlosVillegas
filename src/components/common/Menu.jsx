54e1b5879d01cb4aadd3c9a173747faddccab1bf
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-03-16 23:15:09 -0300

    arregle maquetado del menu para que sea responsive

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,27 +1,35 @@
-import { Col, Row } from 'react-bootstrap';
+
+import { Col } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const Menu = () => {
     return (
-        <section className='text-primary container'>
-          <Container>
-        <Navbar bg="white" variant="dark">
-            <Col lg={4}>
-            <Navbar.Brand className='text-start text-dark' href="#home">Navbar</Navbar.Brand>
-            </Col>
-            <Col lg={8} className="text-end">
-            <Nav className="justify-content-start">
-              <Nav.Link className='text-dark' href="#home">Inicio</Nav.Link>
-              <Nav.Link className='text-dark' href="#features">Servicio</Nav.Link>
-              <Nav.Link className='text-dark' href="#pricing">FAQ</Nav.Link>
-              <Nav.Link className='text-dark' href="#pricing">Galeria</Nav.Link>
-              <Nav.Link className='text-dark' href="#pricing">Contacto</Nav.Link>
-            </Nav>
-            </Col>
-        </Navbar>
-          </Container>
+        <section className='container fs-6'>
+      <Container>
+          <Navbar bg="white" expand="lg"  className="row fs-5 justify-content-between">
+        <Col md={12} lg={5} className="text-center text-lg-start pb-4">
+        <Navbar.Brand href="#">Icono</Navbar.Brand>
+        </Col>
+        <Col md={12} lg={5} className="text-center">
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Inicio</Nav.Link>
+            <Nav.Link href="#action2">Servicios</Nav.Link>
+            <Nav.Link href="#action2">FAQ</Nav.Link>
+            <Nav.Link href="#action2">Galeria</Nav.Link>
+            <Nav.Link href="#action2">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Col>
+    </Navbar>
+      </Container>
         </section>
     );
 };
