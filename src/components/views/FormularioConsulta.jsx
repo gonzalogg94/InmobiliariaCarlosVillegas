7e92911c81e0e63fed5a7930339e7c82d04ae812
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-03-19 20:41:04 -0300

    instale la libreria sweet alert y realice modificacion en la funcionalidad del formulario

--- src/components/views/FormularioConsulta.jsx
+++ src/components/views/FormularioConsulta.jsx
@@ -1,27 +1,36 @@
 import { Container, Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import emailjs from "@emailjs/browser";
-import React, {useRef} from 'react';
+import React, { useRef } from "react";
+import Swal from "sweetalert2";
 
 const FormularioConsulta = () => {
- 
-const refForm=useRef();
+  const refForm = useRef();
 
-const handleSubmit=(event)=>{
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-event.preventDefault();
-console.log(refForm.current)
-const serviceId='service_3xcx39y';
-const templated='template_4gccdsn';
-// 3 parametro
-const apiKey='fDrf7ib8tZNU5G21n'
+    console.log(refForm.current);
+    const serviceId = "service_3xcx39y";
+    const templated = "template_4gccdsn";
 
-emailjs.sendForm(serviceId,templated,refForm.current,apiKey)
-.then(result=>console.log(result))
-.catch(error=>console.log(error))
+    const apiKey = "fDrf7ib8tZNU5G21n";
 
-}
- 
+    emailjs.sendForm(serviceId, templated, refForm.current, apiKey)
+      .then((result) =>{
+        if(result.status===200){
+          Swal.fire({
+            icon: 'success',
+            title: 'OK',
+            text: 'Su consulta se envio satisfactoriamente',
+          })
+        }else{
+
+        }
+
+      });
+  };
+
   return (
     <section id="contacto" className="container-fluid py-4 colorFondo">
       <div className="text-center pb-2 container">
@@ -38,27 +47,39 @@ emailjs.sendForm(serviceId,templated,refForm.current,apiKey)
         <i class="bi bi-whatsapp">+543816254879</i>
       </div>
       <Container className="pt-4">
- <Form ref={refForm} onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Ingrese su nombre completo</Form.Label>
-        <Form.Control name="formNombre" type="text" placeholder="nombre completo" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Ingrese su numero de contacto</Form.Label>
-        <Form.Control name="formNumero" type="number" placeholder="codigo de pais seguido de su codigo de area " />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Ingrese un mail de contacto</Form.Label>
-        <Form.Control name="formMail" type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Ingrese su consulta</Form.Label>
-        <Form.Control name="formConsulta" as="textarea" rows={3} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Enviar
-      </Button>
-    </Form>
+        <Form ref={refForm} onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Ingrese su nombre completo</Form.Label>
+            <Form.Control
+              name="formNombre"
+              type="text"
+              placeholder="nombre completo"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Ingrese su numero de contacto</Form.Label>
+            <Form.Control
+              name="formNumero"
+              type="text"
+              placeholder="codigo de pais seguido de su codigo de area "
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Ingrese un mail de contacto</Form.Label>
+            <Form.Control
+              name="formMail"
+              type="email"
+              placeholder="name@example.com"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Ingrese su consulta</Form.Label>
+            <Form.Control name="formConsulta" as="textarea" rows={3} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form>
       </Container>
     </section>
   );
