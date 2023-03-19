a20476b99e5808ce7002d6a4b101810464a0ec20
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-03-19 00:09:38 -0300

    agregue avances al formulario falta terminar funcionalidad

--- src/components/views/FormularioConsulta.jsx
+++ src/components/views/FormularioConsulta.jsx
@@ -1,6 +1,15 @@
-import { Container,Button } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
+import emailjs from "@emailjs/browser";
+
 const FormularioConsulta = () => {
+  const sendEmail=(event)=>{
+    event.preventDefault();
+
+emailjs.sendForm('service_3xcx39y','template_4gccdsn',event.target,'fDrf7ib8tZNU5G21n')
+.then(response=>console.log(response))
+.catch(error=>console.log(error))
+  }
   return (
     <section id="contacto" className="container-fluid py-4 colorFondo">
       <div className="text-center pb-2 container">
@@ -8,32 +17,35 @@ const FormularioConsulta = () => {
         <hr />
       </div>
       <div className="text-center">
-      <i class="bi bi-envelope">inmobiliariavillegas@gmail.com</i>
+        <i class="bi bi-envelope">inmobiliariavillegas@gmail.com</i>
       </div>
       <div className="text-center">
-      <i class="bi bi-instagram">InmobiliariaVillegas</i>
+        <i class="bi bi-instagram">InmobiliariaVillegas</i>
       </div>
       <div className="text-center">
-      <i class="bi bi-whatsapp">+543816254879</i>
+        <i class="bi bi-whatsapp">+543816254879</i>
       </div>
       <Container className="pt-4">
-        <Form >
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form onSubmit={sendEmail}>
+          <Form.Group className="mb-3" controlId="formNombre">
+            <Form.Label>Nombre completo</Form.Label>
+            <Form.Control type="text" placeholder="Nombre Completo" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formMail">
             <Form.Label>Ingrese un mail de contacto</Form.Label>
             <Form.Control type="email" placeholder="name@example.com" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className="mb-3" controlId="formNumero">
             <Form.Label>Ingrese un numero de contacto</Form.Label>
             <Form.Control type="number" placeholder="+543815768940" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Group className="mb-3" controlId="formConsulta">
             <Form.Label>Ingrese su consulta</Form.Label>
             <Form.Control as="textarea" rows={3} />
           </Form.Group>
           <Button variant="primary" type="submit">
-        Enviar Consulta
-      </Button>
-          
+            Enviar Consulta
+          </Button>
         </Form>
       </Container>
     </section>
