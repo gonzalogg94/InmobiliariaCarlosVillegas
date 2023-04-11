0f72b8ebaf4459dd8e02351bf9c5fa51dec0bd85
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-04-11 20:05:25 -0300

    agregue cambios en el apartado de preguntas frecuentes

--- src/components/views/Preguntas.jsx
+++ src/components/views/Preguntas.jsx
@@ -8,27 +8,15 @@ const Preguntas = () => {
       <Container>
         <Accordion defaultActiveKey="0" flush>
           <Accordion.Item eventKey="0">
-            <Accordion.Header>¿Que disponibilidad tiene?</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <Accordion.Header>¿A que nos dedicamos?</Accordion.Header>
+            <Accordion.Body className="text-start">
+            Proyectos e inversiones en el mundo corrientes al comercio internacional como exportar , como importar , como conseguir mercados, etc.
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
             <Accordion.Header>¿Como puedo contactarme?</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <Accordion.Body className="text-start">
+            Podes contactarte con nosotros a través de nuestras redes sociales o bien podes hacerlo desde nuestra pagina web en el apartado de contacto donde intentaremos asesorarte y brindarte una respuesta lo más rápido posible.
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
